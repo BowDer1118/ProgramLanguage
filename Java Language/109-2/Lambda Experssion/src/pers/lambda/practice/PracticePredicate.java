3d4aebaa3399812db2cc170df5a2c0979e6b35d8
BowDer <blue1118.ss@gmail.com>
2021-04-04 12:00:44 +0800

    Update PracticePredicate class

--- Java Language/109-2/Lambda Experssion/src/pers/lambda/practice/PracticePredicate.java
+++ Java Language/109-2/Lambda Experssion/src/pers/lambda/practice/PracticePredicate.java
@@ -5,18 +5,18 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class PracticePredicate {
-    public static void filter(List<String> list, Predicate<String> p){
-        System.out.println("集合中的元素有 "+list.toString());
-        System.out.println("集合中包含'a'的元素為:");
-        for (String s: list) {
-            if(p.test(s)){
-                System.out.print(s+" ");
-            }
+    public static void filter(List<String> list,Predicate<String> p){
+        System.out.println("集合中的元素有: "+list.toString());
+        System.out.println("集合中包含字符'a'的元素有:");
+        for (int i = 0; i < list.size(); i++) {
+          if(p.test(list.get(i))){
+              System.out.print(list.get(i)+" ");
+          }
         }
         System.out.println();
     }
     public static void main(String[] args) {
-        List<String> list= Arrays.asList("java","python","php","HTML");
+        List<String> list=Arrays.asList("java","python","php","HTML");
         filter(list,s->s.contains("a"));
     }
 }
