6f58df51a4c6e1b5c4aa190c98478cc3f2d03bb8
BowDer1118 <55616135+BowDer1118@users.noreply.github.com>
2022-09-28 00:54:24 +0800

    The logic proof of Record_Last_four_Number
    As title

---
+++ MicroProcessorMachine/[HW02]Seven_Segment_Display/homework/seven_segment/record_last_four_number.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#define NUMBER 4
+
+int values[NUMBER];
+int count;
+int input;
+
+void init() {
+    for (int i = 0; i < NUMBER; i++) {
+        values[i] = 0;
+    }
+    count = 0;
+}
+
+void addValue(int input) {
+    printf("Add:%d\n", input);
+    values[(count - 1) % NUMBER] = input;
+}
+
+void display() {
+    printf("Current array\n");
+    for (int i = 0; i < NUMBER; i++) {
+        printf("%d ", values[i]);
+    }
+    printf("\n");
+
+    printf("Output\n");
+    if (count < NUMBER) {
+        for (int i = 0; i < count; i++) {
+            printf("%d ", values[i]);
+        }
+    } else {
+        for (int i = 0; i < 4; i++) {
+            printf("%d ", values[(count + i) % NUMBER]);
+        }
+    }
+
+    printf("\n\n");
+}
+
+int main() {
+    while (scanf("%d", &input)) {
+        if (input < 0) {
+            break;
+        }
+        count++;
+        addValue(input);
+        display();
+    }
+    return 0;
+}
