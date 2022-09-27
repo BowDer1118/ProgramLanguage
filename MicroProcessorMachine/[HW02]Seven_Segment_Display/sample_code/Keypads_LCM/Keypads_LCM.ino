102000de1b7730fcb16312b0d92873641e4f96e2
BowDer1118 <55616135+BowDer1118@users.noreply.github.com>
2022-09-27 12:01:13 +0800

    Start uploading MicroProcessorMachine code
    As title

---
+++ MicroProcessorMachine/[HW02]Seven_Segment_Display/sample_code/Keypads_LCM/Keypads_LCM.ino
@@ -0,0 +1,234 @@
+//Sample using LiquidCrystal library
+#include <LiquidCrystal.h>
+
+/*******************************************************
+
+This program will test the LCD panel and the buttons
+Mark Bramwell, July 2010
+
+********************************************************/
+
+// select the pins used on the LCD panel
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+char KeyValue[]={'1','2','3','A','4','5','6','B','7','8','9','C','*','0','#','D'};
+byte Row=0, Col=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(10, INPUT); //R1: S1,S2,S3,S4 (1,2,3,A)                                   
+  pinMode(11, INPUT_PULLUP); //R2: S5,S6,S7,S8 (4,5,6,B)
+  pinMode(12, INPUT_PULLUP); //R3: S9, S10, S11,S12 (7,8,9,C)
+  pinMode(13, INPUT_PULLUP); //R4: (*,0,#,D)
+  pinMode(A0, OUTPUT); //A1, C1: S1,S5,S9 (1,4,7,*)
+  pinMode(A1, OUTPUT); //A2, C2: S2,S6,S10 (2,5,8,0)
+  pinMode(A2, OUTPUT); //A3, C3: S3,S7,S11 (3,6,9,#)
+  pinMode(A3, OUTPUT); //A4, C4, S4,S8,S12 (*,0, #,D)  // (A, B, C, D) is correct.
+  //Pin left to right :R1 R2 R3 R4 C1 C2 C3 C4
+
+  digitalWrite(A0,HIGH);
+  digitalWrite(A1,HIGH);
+  digitalWrite(A2,HIGH);
+  digitalWrite(A3,HIGH);
+  lcd.begin(16, 2);              // start the library
+  lcd.setCursor(0,0);
+  for(int i=0; i<3;i++)
+  {
+    lcd.print("Key Martrix Test");
+    delay(1000);
+    lcd.clear();
+    delay(400);
+  }
+} 
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  static int keypressedcount=0;
+  byte keyindex=0;
+  //if key is pressed in the first round scan, 
+  //then call keyscan() again to check if the key pressed in first round is actually pressed 
+  if(keyscan()==true) 
+  {
+    keyindex=(Row-1)*4+Col;
+    delay(5);
+    if ((keyscan()==true) && (keyindex=(Row-1)*4+Col))
+    {
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Row=");lcd.print(Row);
+      lcd.print(",Col=");lcd.print(Col);
+      lcd.setCursor(0,1);
+      lcd.print(KeyValue[keyindex-1]);
+      digitalWrite(A0,LOW);
+      digitalWrite(A1,LOW);
+      digitalWrite(A2,LOW);
+      digitalWrite(A3,LOW);
+      delayMicroseconds(100);
+      while( (digitalRead(2)==LOW) || (digitalRead(3)==LOW))
+        ;  
+    }
+  } 
+}
+
+bool keyscan( )
+{
+  Row=0;Col=0;
+  bool keypressed = false;
+  //scan col1
+  digitalWrite(A0, LOW);
+  digitalWrite(A1, HIGH);
+  digitalWrite(A2, HIGH);
+  digitalWrite(A3, HIGH);
+  delayMicroseconds(100);
+  //Read keys in row.1
+  if(digitalRead(10)==LOW)
+  {
+      digitalWrite(A0, HIGH);
+      Col=1;Row=1;
+      keypressed = true;
+      return(keypressed);
+  }
+  //Read keys in row.2
+  if(digitalRead(11)==LOW)
+  {
+      digitalWrite(A0, HIGH);
+      Col=1;Row=2;
+      keypressed = true;
+      return(keypressed);
+  }
+  //Read keys in row.3
+  if(digitalRead(12)==LOW)
+  {
+      digitalWrite(A0, HIGH);
+      Col=1;Row=3;
+      keypressed = true;
+      return(keypressed);
+  }
+  //Read keys in row.4
+  if(digitalRead(13)==LOW)
+  {
+       digitalWrite(A0, HIGH);
+      Col=1;Row=4;
+      keypressed = true;
+      return(keypressed);
+  }
+  //scan col 2  
+  digitalWrite(A0, HIGH);
+  digitalWrite(A1, LOW);
+  digitalWrite(A2, HIGH);
+  digitalWrite(A3, HIGH);
+  delayMicroseconds(100);
+  //Read keys in row.1
+  if(digitalRead(10)==LOW)
+  {
+      digitalWrite(A1, HIGH);
+      Col=2;Row=1;
+      keypressed = true;
+      return(keypressed);
+  }
+    //Read keys in row.2
+  if(digitalRead(11)==LOW)
+  {
+      digitalWrite(A1, HIGH);
+      Col=2;Row=2;
+      keypressed = true;
+      return(keypressed);
+  }
+  //Read keys in row.3
+  if(digitalRead(12)==LOW)
+  {
+      digitalWrite(A1, HIGH);
+      Col=2;Row=3;
+      keypressed = true;
+      return(keypressed);
+  }
+  //Read keys in row.4
+  if(digitalRead(13)==LOW)
+  {
+      digitalWrite(A1, HIGH);
+      Col=2;Row=4;
+      keypressed = true;
+      return(keypressed);
+  }
+
+  //scan col 3  
+  digitalWrite(A0, HIGH);
+  digitalWrite(A1, HIGH);
+  digitalWrite(A2, LOW);
+  digitalWrite(A3, HIGH);
+  delayMicroseconds(100);
+  //Read keys in row.1
+  if(digitalRead(10)==LOW)
+  {
+      digitalWrite(A2, HIGH);
+      Col=3;Row=1;
+      keypressed = true;
+      return(keypressed);
+  }
+  //Read keys in row.2
+  if(digitalRead(11)==LOW)
+  {
+      digitalWrite(A2, HIGH);
+      Col=3;Row=2;
+      keypressed = true;
+      return(keypressed);
+  }  
+  //Read keys in row.3
+  if(digitalRead(12)==LOW)
+  {
+      digitalWrite(A2, HIGH);
+      Col=3;Row=3;
+      keypressed = true;
+      return(keypressed);
+  }
+  //Read keys in row.4
+  if(digitalRead(13)==LOW)
+  {
+      digitalWrite(A2, HIGH);
+      Col=3;Row=4;
+      keypressed = true;
+      return(keypressed);
+  }
+  
+  //scan col 4  
+  digitalWrite(A0, HIGH);
+  digitalWrite(A1, HIGH);
+  digitalWrite(A2, HIGH);
+  digitalWrite(A3, LOW);
+  delayMicroseconds(100);
+  //Read keys in row.1
+  if(digitalRead(10)==LOW)
+  {
+      digitalWrite(A3, HIGH);
+      Col=4;Row=1;
+      keypressed = true;
+      return(keypressed);
+  }
+    //Read keys in row.2
+  if(digitalRead(11)==LOW)
+  {
+      digitalWrite(A3, HIGH);
+      Col=4;Row=2;
+      keypressed = true;
+      return(keypressed);
+  }
+  //Read keys in row.3
+  if(digitalRead(12)==LOW)
+  {
+      digitalWrite(A3, HIGH);
+      Col=4;Row=3;
+      keypressed = true;
+      return(keypressed);
+  }
+  //Read keys in row.4
+  if(digitalRead(13)==LOW)
+  {
+      digitalWrite(A3, HIGH);
+      Col=4;Row=4;
+      keypressed = true;
+      return(keypressed);
+  }
+  return(false);
+}
+
+
+
---
+++ MicroProcessorMachine/[HW02]Seven_Segment_Display/sample_code/test_7Seg/test_7Seg.ino
@@ -0,0 +1,94 @@
+/* Define shift register pins used for seven segment display */
+#define LATCH_DIO D15   //電路圖:控制暫存器要在何時工作的CLOCK LCHCLK的腳位是D15 (racing edge triggered or falling edge triggered)
+#define CLK_DIO D14     //電路圖:控制資料輸入的CLOCK SFTCLK的腳位
+#define DATA_DIO D2     //電路圖:資料輸入的腳位D2
+#define BUTTON1 BT1     //母板BTN1
+#define BUTTON2 BT2     //母板BTN2
+#define BUTTON3 BT3     //母板BTN3
+#define BUTTON4 BT4     //母板BTN4
+#define BUTTON_A0 A0   //對應key *
+#define BUTTON_A1 A1   //對應key 0
+#define BUTTON_A2 A2   //對應key #
+#define BUTTON_A3 A3   //對應key D
+
+
+/* Segment byte maps for numbers 0 to 9 , A, b , C*/ // MSB: dp g f e d c b a :LSB 參考7-segment圖形
+const byte SEGMENT_MAP[] = {0x3F, 0x06, 0x3C, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0X7F, 0X6F, 0X77, 0X7C, 0X39};
+const byte DP = 0x80;   // Digit Point
+
+/* Byte maps to select digit 1 to 4 */ // E: 1110  D: 1101  B: 1011   7: 0111    // the 0's place (左到右分別為 E D B 7)
+const byte SEGMENT_SELECT[] = {0x0E, 0x0D, 0x0B, 0x07}; // {0xFE,0xFD,0xFB,0xF7};  is OK, too.
+
+/* Write a decimal number between 0 and 9 to one of the 4 digits of the display */
+void WriteNumberToSegment(byte Segment, byte Value) //使用正向觸發的方式傳輸資料(LATCH_DIO->先給LOW->傳輸資料->再給HIGH)
+{
+  digitalWrite(LATCH_DIO, LOW);
+  if (Value == -1)
+  {
+    shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, 0xFF); //清除顯示器內容
+  } else
+  {
+    // The following order cannot be changed. MAP first and then SELECT. (因電路設計先傳輸要輸出的數字資料再傳輸要顯示的位置資料)
+    //shiftOut(資料傳輸腳位,資料傳輸的clock腳位,資料傳輸方式,要傳輸的資料)
+    shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, SEGMENT_MAP[Value]); //要顯示的圖案(數值)
+    shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, SEGMENT_SELECT[Segment] ); //要顯示的位置(7-segment的位置)
+  }
+  digitalWrite(LATCH_DIO, HIGH);
+}
+
+void setup ()
+{
+  /* Set DIO pins to outputs */
+  pinMode(LATCH_DIO,  OUTPUT);
+  pinMode(CLK_DIO,  OUTPUT);
+  pinMode(DATA_DIO, OUTPUT);
+
+  //清除顯示器
+  WriteNumberToSegment(0, -1);
+  WriteNumberToSegment(1, -1);
+  WriteNumberToSegment(2, -1);
+  WriteNumberToSegment(3, -1);
+}
+
+/* Main program */
+void loop()
+{
+  /* Update the display with the current counter value */
+  //在讀取按鈕時 當按鈕被按下 會讀到的數值會是0 (原因:看電路圖 當按鈕按下時電路將導通接地 電位會輸出0)
+  if (!digitalRead(BUTTON1))
+  {
+    WriteNumberToSegment(3 , 1);
+  }
+  if (!digitalRead(BUTTON2))
+  {
+    WriteNumberToSegment(2 , 3);
+  }
+  if (!digitalRead(BUTTON3))
+  {
+    WriteNumberToSegment(1 , 5);
+  }
+  if (!digitalRead(BUTTON4))
+  {
+    WriteNumberToSegment(0 , 7);
+  }
+  if (!digitalRead(BUTTON_A0))
+  {
+    WriteNumberToSegment(0 , 9);
+  }
+  if (!digitalRead(BUTTON_A1))
+  {
+    WriteNumberToSegment(1 , 10);
+  }
+  if (!digitalRead(BUTTON_A2))
+  {
+    WriteNumberToSegment(2 , 11);
+  }
+  if (!digitalRead(BUTTON_A3))
+  {
+    WriteNumberToSegment(3 , 12);
+  }
+}
+
+
+
+
