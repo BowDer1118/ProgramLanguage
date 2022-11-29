5999262840db3d0bd50120f6b7167690f4bc3d90
BowDer1118 <55616135+BowDer1118@users.noreply.github.com>
2022-11-29 08:59:35 +0800

    Update RTC_Counter.ino

--- MicroProcessorMachine/[HW05]RTC_Counter/RTC_Counter.ino
+++ MicroProcessorMachine/[HW05]RTC_Counter/RTC_Counter.ino
@@ -22,76 +22,78 @@ String countingState;
 int countingHour, countingMin, countingSecond;
 
 void setup() {
-    /*BT1~BT4*/
-    pinMode(BT1, INPUT);
-    pinMode(BT2, INPUT);
-    pinMode(BT3, INPUT);
-    pinMode(BT4, INPUT);
+  /*BT1~BT4*/
+  pinMode(BT1, INPUT);
+  pinMode(BT2, INPUT);
+  pinMode(BT3, INPUT);
+  pinMode(BT4, INPUT);
 
-    // set up the LCD's number of columns and rows
-    lcd.begin(16, 2);
+  // set up the LCD's number of columns and rows
+  lcd.begin(16, 2);
 
-    // set the time and start counting
-    RTC.settime(day, hours, minutes, seconds);
-    RTC.startcounting();
+  // set the time and start counting
+  RTC.settime(day, hours, minutes, seconds);
+  RTC.startcounting();
 
-    isEnable = false;
+  isEnable = false;
 }
 
 void loop() {
-    hours = RTC.gethours();
-    minutes = RTC.getminutes();
-    seconds = RTC.getseconds();
-    day = RTC.getday();
+  hours = RTC.gethours();
+  minutes = RTC.getminutes();
+  seconds = RTC.getseconds();
+  day = RTC.getday();
 
-    buttonState1 = digitalRead(BT1);
-    buttonState2 = digitalRead(BT2);
-    buttonState3 = digitalRead(BT3);
-    buttonState4 = digitalRead(BT4);
+  buttonState1 = digitalRead(BT1);
+  buttonState2 = digitalRead(BT2);
+  buttonState3 = digitalRead(BT3);
+  buttonState4 = digitalRead(BT4);
 
-    /*輸出時間*/
-    lcd.setCursor(0, 0);
-    //使用sprintf進行字串格式轉換
-    sprintf(timeBuffer, "%02d/%02d   %02d:%02d:%02d", month, day, hours, minutes, seconds);
-    lcd.print(timeBuffer);
+  /*輸出時間*/
+  lcd.setCursor(0, 0);
+  //使用sprintf進行字串格式轉換
+  sprintf(timeBuffer, "%02d/%02d   %02d:%02d:%02d", month, day, hours, minutes, seconds);
+  lcd.print(timeBuffer);
 
-    /*檢查按鈕是否被按下*/
-    if (buttonState1 == LOW) {
-        isEnable = true;
-        countingState = "StopWatch";
-        countingSecond = 0;
-        countingMin = 0;
-        countingHour = 0;
-    } else if (buttonState2 == LOW) {
-        countingState = "StopWatch";
-        countingSecond = 0;
-        countingMin = 0;
-        countingHour = 0;
-    } else if (buttonState3 == LOW) {
-        isEnable = false;
-        countingSecond = 0;
-        countingMin = 0;
-        countingHour = 0;
-    } else if (buttonState4 == LOW) {
-        if (countingState != "Counting") {
-            countingState = "Counting";
-        } else {
-            countingState = "Stop";
-        }
-
-        if (isEnable) {
-            if (countingState == "Counting") {
-                /*碼表時間處理*/
-                countingSecond += 1;
-                countingMin += int(countingSecond / 60);
-                countingSecond %= 60;
-                countingHour += int(countingMin / 60);
-                countingMin %= 60;
-                countingHour %= 24;
-            }
-            sprintf(timeBuffer, "%2d:%2d:%2d %s", countingHour, countingMin, countingSecond, countingState.c_str());
-        }
-
-        delay(1000);
+  /*檢查按鈕是否被按下*/
+  if (buttonState1 == LOW) {
+    isEnable = true;
+    countingState = "StopWatch";
+    countingSecond = 0;
+    countingMin = 0;
+    countingHour = 0;
+  } else if (buttonState2 == LOW) {
+    countingState = "StopWatch";
+    countingSecond = 0;
+    countingMin = 0;
+    countingHour = 0;
+  } else if (buttonState3 == LOW) {
+    isEnable = false;
+    countingSecond = 0;
+    countingMin = 0;
+    countingHour = 0;
+  } else if (buttonState4 == LOW) {
+    if (countingState != "Counting") {
+      countingState = "Counting";
+    } else {
+      countingState = "Stop";
     }
+  }
+
+  if (isEnable) {
+    if (countingState == "Counting") {
+      /*碼表時間處理*/
+      countingSecond += 1;
+      countingMin += int(countingSecond / 60);
+      countingSecond %= 60;
+      countingHour += int(countingMin / 60);
+      countingMin %= 60;
+      countingHour %= 24;
+    }
+    sprintf(timeBuffer, "%2d:%2d:%2d %s", countingHour, countingMin, countingSecond, countingState.c_str());
+    lcd.setCursor(0, 1);
+    lcd.print(timeBuffer);
+  }
+
+  delay(1000);
 }
